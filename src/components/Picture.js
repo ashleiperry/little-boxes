9fcce9eef8625986c73b31a89915943c6416dff7
Ashlei Perry <ashlei.perry@gmail.com>
2017-11-10 18:00:20 -0500

    made this more dynamic. also, spent plenty of time practicing iterating over arrays with React components.

--- src/components/Picture.js
+++ src/components/Picture.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import List from './List'; 
+import List from './List';
 const Picture = props => {
+  let listo=["Asteroids", "Comets", "Moon", "Planets", "Stars", "Sun"]
   return (
     <div className='box picture'>
       <h1>Look at This Picture</h1>
-      <img alt='Clever EEs' src='https://s3.amazonaws.com/horizon-production/images/react-clever-ees.png'/>
-      <List />
+      <img alt='Clever EEs' src={props.src}/>
+      <List listo= {listo}/>
     </div>
   )
 }
